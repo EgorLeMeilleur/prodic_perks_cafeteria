ecf30c28bddf3e0a08d16b44e7812295e4cd6fc5
Ivan Urbanovich <urbanovich.io@phystech.edu>
2023-03-27 18:50:25 +0500

    правки по авторизации

--- Worker/forms.py
+++ Worker/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'login'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'password'}))
+    username = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class': 'login'}))
+    password = forms.CharField(label="", widget=forms.PasswordInput(attrs={'class': 'password'}))
