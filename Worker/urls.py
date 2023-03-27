31d737d1a3a6e95840c485eebccef604f4ab9fee
egorlemeilleur <pokumin.ga@phystech.edu>
2023-03-27 18:20:43 +0500

    authorization

--- Worker/urls.py
+++ Worker/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('auth/', views.home, name='home'),
-    path('auth/personal_cabinet/', views.personal_cabinet, name='personal_cabinet'),
+    path('personal_cabinet/', views.personal_cabinet, name='personal_cabinet'),
     path('logout/', views.logout, name='logout')
 ]
